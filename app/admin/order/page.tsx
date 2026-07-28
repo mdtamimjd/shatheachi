c42b6b2c440f1a50ebc33d256bb3baa2ae1d0f9d
mdtamimjd <tamimoli75285@gmail.com>
2026-07-28 16:08:45 +0600

    order page update

--- app/admin/order/page.tsx
+++ app/admin/order/page.tsx
@@ -2,24 +2,27 @@ import OrderItem from "@/components/OrderItem";
 import dbConnect from "@/config/db";
 import orderModel from "@/models/Order";
 
+interface TItems { product: any; quantity: number; }
+export interface TOrder { customer_info: { fullName: string; phoneNumber: string; address: string; selectAddress: { division?: string; district?: string; upazila?: string; union?: string; } }, items: TItems[], amount: { itemsPrice: number, deliveryCharge: number, totalAmount: number, }, status: string, createdAt: string, updatedAt: string, _id: string, __v?: number }
+
 export default async function Page() {
-  await dbConnect();
+    await dbConnect();
 
-  const data = await orderModel
-    .find()
-    .populate("items.product")
-    .lean();
+    const data = await orderModel
+        .find()
+        .populate("items.product")
+        .lean();
 
-  return (
-    <div className="p-5 space-y-3 xl:max-w-7xl mx-auto">
-      <section className="space-y-5">
-        {data.map((d: any) => (
-          <OrderItem
-            key={d._id.toString()}
-            data={JSON.parse(JSON.stringify(d))}
-          />
-        ))}
-      </section>
-    </div>
-  );
+    return (
+        <div className="p-5 space-y-3 xl:max-w-7xl mx-auto">
+            <section className="space-y-5">
+                {data.map((d: any) => (
+                    <OrderItem
+                        key={d._id.toString()}
+                        data={JSON.parse(JSON.stringify(d))}
+                    />
+                ))}
+            </section>
+        </div>
+    );
 }
